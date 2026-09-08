7c55f2cc3592ad86950897d799ddc8ff949e1ff0
QianChang <qianchanglys@gmail.com>
2026-09-08 08:57:14 +0800

    docs(competition): build_review_docx.py 补木兰版权头（license_header --apply，402/402 通过） (#219)

--- competition/build_review_docx.py
+++ competition/build_review_docx.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+# Copyright (c) 2026 QianChang-official
+#
+# 宛委·枢忆 is licensed under Mulan PSL v2.
+# You can use this software according to the terms of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+# http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
 """将 competition/评审报告.md 导出为评委版 Word 文档。
 
 排版规范（Issue #216）：全文微软雅黑；正文小四（12pt）；标题加粗放大；
